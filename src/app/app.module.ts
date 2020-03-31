37e3c8f152cb846e643d491ab7476ebda2e0cacf
Remi Kristelijn <remi.kristelijn@theapsgroup.com>
2020-03-31 16:48:05 +0200

    feat: add store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -9,6 +10,7 @@ import { ShellModule } from './modules/shell/shell.module'
 import { DashboardModule } from './modules/dashboard/dashboard.module'
 
 import { appReducer } from './app.reducer'
+import { environment } from 'src/environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +21,11 @@ import { appReducer } from './app.reducer'
     ShellModule,
     DashboardModule,
     StoreModule.forRoot(appReducer),
+    StoreDevtoolsModule.instrument({
+      name: 'My App',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [ShellModule],
   bootstrap: [AppComponent],
