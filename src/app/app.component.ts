f1095325b09c34c2ec28093cc1f578d0ae5da510
Remi Kristelijn <remi.kristelijn@theapsgroup.com>
2020-03-31 12:05:35 +0200

    refactor: clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<div>hello world</div>`,
 })
 export class AppComponent {
-  title = 'ngrx-playground';
+  title = 'ngrx-playground'
 }
